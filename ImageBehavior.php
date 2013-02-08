ec6e25f2c7c8fd55ba49f0c67700430c582c01df
Andrew <andrew@power-marketing.com>
2013-02-08 13:31:33 -0500

    setup() formatting updated

--- ImageBehavior.php
+++ ImageBehavior.php
@@ -22,7 +22,7 @@ class ImageBehavior extends ModelBehavior {
         public function setup(Model $Model, $settings = array()) {
             if (!isset($this->settings[$Model->alias])) {
                 $this->settings[$Model->alias] = array(
-                        'path' => '',  //Path from webroot. If it begins with a slah HTML helper assumes absoloute. otherwise it serves from the 'img' directory in webroot  
+                        'path' => '',  //Path from webroot. If it begins with a slah, HTML helper assumes an absoloute path. Otherwise, it serves from the 'img' directory in webroot  
                         'full_path' => IMAGES,// TODO Set to false to use db for storage
                         'uuid' => 'uuid',
                         'extention' => 'ext',
@@ -33,10 +33,9 @@ class ImageBehavior extends ModelBehavior {
             }
             $this->settings[$Model->alias] = array_merge(
                 $this->settings[$Model->alias], (array)$settings);
-            
                 $Model->virtualFields = array(
-                'path' => "CONCAT('{$this->settings[$Model->alias]['path']}', {$Model->alias}.{$this->settings[$Model->alias]['name_field']}, '.', {$Model->alias}.{$this->settings[$Model->alias]['extention_field']})",
-                'full_path' => "CONCAT({$this->settings[$Model->alias]['full_path']}, {$Model->alias}.{$this->settings[$Model->alias]['name_field']}, '.', {$Model->alias}.{$this->settings[$Model->alias]['extention_field']})"
+                'path'      => "CONCAT('{$this->settings[$Model->alias]['path']}',      {$Model->alias}.{$this->settings[$Model->alias]['name_field']}, '.', {$Model->alias}.{$this->settings[$Model->alias]['extention_field']})",
+                'full_path' => "CONCAT('{$this->settings[$Model->alias]['full_path']}', {$Model->alias}.{$this->settings[$Model->alias]['name_field']}, '.', {$Model->alias}.{$this->settings[$Model->alias]['extention_field']})"
                 );
         }
 
